80e5da47404c900b55af12c3e4f0d0b8fc63ccac
CécileH <97221105+CecileHonnorat@users.noreply.github.com>
2022-06-28 16:46:02 +0200

    server

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 module.exports = app;
