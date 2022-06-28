aec74b1842782fc17e94ecf9cd319ccdbc7bf9b8
CécileH <97221105+CecileHonnorat@users.noreply.github.com>
2022-06-28 16:08:05 +0200

    build deploy

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ class Server {
     // Catch all requests that don't match any route
     this.app.get("*", (req, res) => {
       res.sendFile(
-        path.join(__dirname, "client", "build", "index.html")
+        path.join(__dirname, '../client/build')
       );
     });
   }
